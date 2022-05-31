e5f5e9a8d25d41ab28188a48f33d520d6f0b26f4
ThinkPad T470 <belkoegor1@gmail.com>
2022-05-31 14:27:04 +0300

    Extensions

---
+++ app/src/main/java/com/example/weather001/ui/main/Extension.kt
@@ -0,0 +1,39 @@
+package com.example.weather001.ui.main
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.core.content.getSystemService
+import java.lang.RuntimeException
+import java.util.Date
+
+fun String.isContainsA() {
+    this.contains("a")
+}
+
+fun Date.yesterdayTime(): Long {
+    return this.time - 24*60*60*1000
+}
+
+class Extensions() {
+    fun yesterdayTime(date: Date): Long {
+        return date.time - 24*60*60*1000
+    }
+}
+
+fun EditText.showKeyboard(){
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as
+            InputMethodManager
+    this.requestFocus()
+    imm.showSoftInput(this, 0)
+}
+
+fun EditText.hideKeyboard(): Boolean{
+    try {
+        val inputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        return  inputMethodManager.hideSoftInputFromWindow(windowToken,0)
+    } catch (ignored: RuntimeException){}
+    return false
+}
