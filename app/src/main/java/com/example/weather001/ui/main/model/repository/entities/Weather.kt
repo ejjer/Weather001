a380aecb0c0ee1edb29636a190a2c5ace31502c5
ThinkPad T470 <belkoegor1@gmail.com>
2022-06-02 13:55:32 +0300

    переделываю getWeatherFromServer

--- app/src/main/java/com/example/weather001/ui/main/model/repository/entities/Weather.kt
+++ app/src/main/java/com/example/weather001/ui/main/model/repository/entities/Weather.kt
@@ -7,7 +7,8 @@ import kotlinx.parcelize.Parcelize
 data class Weather(
     val city: City = getDefaultCity(),
     val temperature: Int = 0,
-    val feelsLike: Int = 0
+    val feelsLike: Int = 0,
+    val condition: String? = "Неизвестно"
 ) : Parcelable
 
 fun getDefaultCity() = City("Москва", 55.755826, 37.617299900000035)
