ba2275224d6e3f7602d5594ef7e1db2a69383cad
ThinkPad T470 <belkoegor1@gmail.com>
2022-06-10 12:37:17 +0300

    переводим запрос на ретрофит

--- app/src/main/java/com/example/weather001/ui/main/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/weather001/ui/main/model/repository/RepositoryImpl.kt
@@ -4,10 +4,11 @@ import com.example.weather001.ui.main.model.repository.entities.Weather
 import com.example.weather001.ui.main.model.repository.entities.getRussianCities
 import com.example.weather001.ui.main.model.repository.entities.getWorldCities
 import com.example.weather001.ui.main.model.repository.WeatherLoader
+import com.example.weather001.ui.main.model.repository.rest.WeatherRepo
 
 class RepositoryImpl : Repository {
     override fun getWeatherFromServer(lat: Double, lon: Double): Weather {
-        val dto = WeatherLoader.loadWeather(lat, lon)
+        val dto = WeatherRepo.api.getWeather(lat, lon).execute().body()
         return Weather(
             temperature = dto?.fact?.temp ?: 0,
             feelsLike = dto?.fact?.feelsLike ?: 0,
