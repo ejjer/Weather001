19dbbe2b4d712e4efd73f7aa967107efdffe650c
ThinkPad T470 <belkoegor1@gmail.com>
2022-06-17 20:07:26 +0300

    добавил в меню

--- app/src/main/java/com/example/weather001/MainActivity.kt
+++ app/src/main/java/com/example/weather001/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.example.weather001.ui.main.MainFragment
+import com.example.weather001.ui.main.contacts.ContactsFragment
 import com.example.weather001.ui.main.history.HistoryFragment
 import com.example.weather001.ui.main.theads.ThreadsFragment
 
@@ -34,6 +35,10 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.menu_history -> {
                 openFragment(HistoryFragment.newInstance())
+                true
+            }
+            R.id.menu_content_provider->{
+                openFragment(ContactsFragment.newInstance())
                 true
             }
             else -> super.onOptionsItemSelected(item)
