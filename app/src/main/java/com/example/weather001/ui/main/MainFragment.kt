eff79f2f959bb1bfeb5cdd98c597f3eaf1571f40
ThinkPad T470 <belkoegor1@gmail.com>
2022-06-20 15:10:57 +0300

    добавляем кнопку и прописываем ее реализацию

--- app/src/main/java/com/example/weather001/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/weather001/ui/main/MainFragment.kt
@@ -1,11 +1,21 @@
 package com.example.weather001.ui.main
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.example.weather001.R
@@ -13,19 +23,47 @@ import com.example.weather001.databinding.MainFragmentBinding
 import com.example.weather001.ui.main.adapters.MainFragmentAdapter
 import com.example.weather001.ui.main.details.DetailsFragment
 import com.example.weather001.ui.main.model.repository.AppState
+import com.example.weather001.ui.main.model.repository.entities.City
 import com.example.weather001.ui.main.model.repository.entities.Weather
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 private const val dataSetKey = "dataSetKey"
+private const val REFRESH_PERIOD = 1000L
+private const val MINIMAL_DISTANCE = 1f
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), CoroutineScope by MainScope() {
     private val viewModel: MainViewModel by viewModel()
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
 
     private var adapter: MainFragmentAdapter? = null
     private var isDataSetRus = true
+
+    private val permissionResult = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            getLocation()
+        } else {
+            Toast.makeText(
+                context,
+                getString(R.string.dialog_message_no_gps),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private val onLocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            context?.let {
+                getAddressAsync(it, location)
+            }
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,6 +79,7 @@ class MainFragment : Fragment() {
         with(binding) {
             mainFragmentRecyclerView.adapter = adapter
             mainFragmentFAB.setOnClickListener { changeWeatherDataSet() }
+            mainFragmentFABLocation.setOnClickListener { checkLocationPermission() }
 
             val observer = Observer<AppState> { renderData(it) }
             viewModel.liveData.observe(viewLifecycleOwner, observer)
@@ -83,6 +122,80 @@ class MainFragment : Fragment() {
         initDataSet()
     }
 
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            getLocation()
+        } else {
+            permissionResult.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Suppress("DEPRECATION")
+    private fun getLocation() {
+        context?.let { context ->
+            val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    REFRESH_PERIOD,
+                    MINIMAL_DISTANCE,
+                    onLocationListener
+                )
+            } else {
+                val location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER)
+                if (location == null) {
+                    Toast.makeText(context, getString(R.string.dialog_title_gps_turned_off), Toast.LENGTH_SHORT).show()
+                } else {
+                    getAddressAsync(context, location)
+                }
+            }
+        }
+    }
+
+    private fun getAddressAsync(context: Context, location: Location) {
+        val geoCoder = Geocoder(context)
+        launch {
+            val job = async(Dispatchers.IO) {
+                geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+            }
+            val addresses = job.await()
+            if (isActive) {
+                showAddressDialog(addresses[0].getAddressLine(0), location)
+            }
+        }
+    }
+
+    private fun showAddressDialog(address: String, location: Location) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_address_title))
+                .setMessage(address)
+                .setPositiveButton(getString(R.string.dialog_address_get_weather)) { _, _ ->
+                    openDetailsFragment(
+                        Weather(City(address, location.latitude, location.longitude))
+                    )
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun openDetailsFragment(weather: Weather) {
+        activity?.supportFragmentManager?.let { manager ->
+            val bundle = Bundle().apply {
+                putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
+            }
+            manager.beginTransaction()
+                .add(R.id.container, DetailsFragment.newInstance(bundle))
+                .addToBackStack("")
+                .commitAllowingStateLoss()
+        }
+    }
+
     private fun renderData(appState: AppState) = with(binding) {
         when (appState) {
             is AppState.Success -> {
@@ -120,6 +233,7 @@ class MainFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        cancel()
         _binding = null
     }
 
