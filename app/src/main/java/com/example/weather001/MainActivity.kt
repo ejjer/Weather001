b8d8137033582d34697dcb2446abf993b6dfc422
ThinkPad T470 <belkoegor1@gmail.com>
2022-06-21 12:39:28 +0300

    добавляем карту

--- app/src/main/java/com/example/weather001/MainActivity.kt
+++ app/src/main/java/com/example/weather001/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.weather001.ui.main.MainFragment
 import com.example.weather001.ui.main.contacts.ContactsFragment
 import com.example.weather001.ui.main.history.HistoryFragment
+import com.example.weather001.ui.main.maps.MapsFragment
 import com.example.weather001.ui.main.theads.ThreadsFragment
 
 class MainActivity : AppCompatActivity() {
@@ -39,6 +40,10 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.menu_content_provider->{
                 openFragment(ContactsFragment.newInstance())
+                true
+            }
+            R.id.menu_google_maps ->{
+                openFragment(MapsFragment.newInstance())
                 true
             }
             else -> super.onOptionsItemSelected(item)
